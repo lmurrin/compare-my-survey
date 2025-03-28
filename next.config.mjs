4ca7d13c4bf84342b79a2839806e964d0817355a
lmurr248 <laurencemurrin@gmail.com>
2025-03-28 14:21:58 +0000

    Demo deployment issue.

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,14 @@
-import path from 'path';
+import path from "path";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   env: {
-    serverUrl: 'http://localhost:8000', // Update with your server URL
-    apiKey: 'ValleyView8646*', // Update with your API key
+    serverUrl: "http://localhost:8000", // Update with your server URL
+    apiKey: "ValleyView8646*", // Update with your API key
   },
   webpack(config) {
     // Add alias configuration
-    config.resolve.alias['@'] = path.resolve(__dirname); // Ensure the alias points to your project root
+    config.resolve.alias["@"] = path.resolve(__dirname, "app");
     return config;
   },
 };
