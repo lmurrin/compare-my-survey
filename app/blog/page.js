bb31778235db4936440bcc7c32163c0050484b6e
lmurrin <laurencemurrin@gmail.com>
2025-03-31 08:47:33 +0100

    Updates to blog archive page.

--- app/blog/page.js
+++ app/blog/page.js
@@ -72,10 +72,10 @@ export default function BlogPage() {
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
         <div className="mx-auto max-w-2xl text-center">
           <h2 className="text-balance text-4xl font-semibold tracking-tight text-gray-900 sm:text-5xl">
-            From the blog
+            Read our articles
           </h2>
           <p className="mt-2 text-lg/8 text-gray-600">
-            Learn how to grow your business with our expert advice.
+            Advice and tips for all things property!
           </p>
         </div>
         <div className="mx-auto mt-16 grid max-w-2xl auto-rows-fr grid-cols-1 gap-8 sm:mt-20 lg:mx-0 lg:max-w-none lg:grid-cols-3">
