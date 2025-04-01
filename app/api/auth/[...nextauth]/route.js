1c43fff2b720480fd0704a14146e64591fb06ba4
lmurrin <laurencemurrin@gmail.com>
2025-04-01 16:50:29 +0100

    Resolve deployment.

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,8 +1,10 @@
 export const dynamic = 'force-dynamic';
 
 import NextAuth from 'next-auth';
-import { authOptions } from '@/lib/authOptions';
 
-const handler = NextAuth(authOptions);
+const handler = async (...args) => {
+  const { authOptions } = await import('@/lib/authOptions');
+  return NextAuth(authOptions)(...args);
+};
 
 export { handler as GET, handler as POST };
